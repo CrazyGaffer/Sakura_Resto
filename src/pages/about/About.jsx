7251da6e0f30501a02ad2d0b60f5d7a885620430
Hobo <62564443+CrazyGaffer@users.noreply.github.com>
2025-03-16 22:38:19 +0200

    Few Updates

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import WorkingHours from './subitems/WorkingHours/WorkingHours.jsx';
 import Statistics from './subitems/Statistics/Statistics.jsx';
@@ -10,16 +11,40 @@ import '/src/components/Divider/Divider.css';
 import '/src/pages/about/subitems/FeatureSection.scss';
 import Divider from "../../components/Divider/Divider.jsx";
 
-
 function About() {
     const { t } = useTranslation();
+    const [showLightbox, setShowLightbox] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const handleImageClick = () => {
+        setShowLightbox(true);
+    };
+
+    const closeLightbox = (e) => {
+        if (e.target.classList.contains('lightbox-overlay')) {
+            setShowLightbox(false);
+        }
+    };
 
     return (
         <section id="about" className="about">
             <div className="about-wrapper">
                 <div className="about-content">
                     <div className="about-image">
-                        <img src={interiorImage} alt="About us" />
+                        <div
+                            className={`image-container ${isLoaded ? 'loaded' : ''}`}
+                            onClick={handleImageClick}
+                            role="button"
+                            aria-label="Enlarge interior image"
+                        >
+                            {!isLoaded && <div className="image-loader"></div>}
+                            <img
+                                src={interiorImage}
+                                alt="About us"
+                                onLoad={() => setIsLoaded(true)}
+                                className="hover-zoom"
+                            />
+                        </div>
                     </div>
                     <div className="about-text">
                         <div className="about-header">
@@ -32,13 +57,37 @@ function About() {
                     </div>
                 </div>
 
+                {showLightbox && (
+                    <div
+                        className="lightbox-overlay"
+                        onClick={closeLightbox}
+                        role="dialog"
+                        aria-modal="true"
+                        aria-label="Enlarged image view"
+                    >
+                        <div className="lightbox-content">
+                            <img
+                                src={interiorImage}
+                                alt="Enlarged interior view"
+                                className="lightbox-image"
+                            />
+                        </div>
+                        <button
+                            className="close-button"
+                            onClick={() => setShowLightbox(false)}
+                            aria-label="Close enlarged view"
+                        >
+                            &times;
+                        </button>
+                    </div>
+                )}
+
                 <Divider />
                 <FeaturesSection />
                 <Divider />
                 <WorkingHours />
                 <Divider />
                 <Statistics />
-
             </div>
         </section>
     );
